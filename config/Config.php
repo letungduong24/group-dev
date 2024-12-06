e09aee3636885e538d396f33a39a7e4f52d2645a
letungduong24echo <2251172311@e.tlu.edu.vn>
2024-12-06 14:27:42 +0700

    ok

--- config/Config.php
+++ config/Config.php
@@ -1,11 +1,4 @@
 <?php
+$appDir = '/mvc';
 define('APP_ROOT', dirname(__FILE__, 2));
-
-$scriptName = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
-$basePath = rtrim(str_replace('/views/admin', '', $scriptName), '/'); // Loại bỏ /views/admin nếu tồn tại và dấu '/' cuối cùng
-
-if ($basePath === '') {
-    $basePath = '/'; 
-}
-
-define('BASE_URL', $basePath);
+define('BASE_URL', '/'); 
